c04ad4666e049332638262fbaf416d0441b9df2a
Yuvraj004 <yuvrajchat@gmail.com>
2024-08-05 18:10:03 +0530

    authprovider and middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
-import { NextRequest,NextResponse } from 'next/server'
-// import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 export {default} from 'next-auth/middleware';
 import { getToken } from 'next-auth/jwt';
 
@@ -19,7 +19,7 @@ export async function middleware(request: NextRequest) {
     ){
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
-  return NextResponse.redirect(new URL('/', request.url))
+  // return NextResponse.redirect(new URL('/home', request.url))
 }
  
 // On which routes middleware wil work

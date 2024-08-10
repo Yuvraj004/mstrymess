8ebcad6170a8d52c64c6622fdee1f79d964d1144
Yuvraj004 <yuvrajchat@gmail.com>
2024-08-10 23:24:40 +0530

    signup page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -10,6 +10,18 @@ import { useRouter } from "next/navigation";
 import { signUpSchema } from "@/schemas/signUpSchema";
 import axios, { AxiosError } from "axios";
 import { ApiResponse } from "@/types/ApiResponse";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import {Loader2} from 'lucide-react';
 const page = () => {
   //fields create krne hain
 
@@ -98,7 +110,71 @@ const page = () => {
           </h1>
         </div>
         <div>
-        
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="username" {...field} onChange={(e) => {
+                      field.onChange(e);
+                      setusername(e.target.value)
+                    }} /> // onChange event was needed becoz we have to use setusername
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+              />
+              <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display email.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+              />
+              <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type='password' placeholder="password" {...field}/>
+                  </FormControl>
+                  <FormDescription>
+                    Add a good password.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+              <Button type="submit" disabled={isSubmitting}>{isSubmitting ? (
+                <>
+                <Loader2 className="mr-3 h-4 w-4 animate-spin"/> Please Wait
+              </>) : ("Signup")}</Button>
+          </form>
+        </Form>
+        </div>
+        <div className="text-center mt-4">
+          <p>
+            Already a member?{' '}
+            <Link href='/sign-in' className="text-blue-600 hover:text-blue-800" >Sign in</Link>
+          </p>
         </div>
       </div>
     </div>

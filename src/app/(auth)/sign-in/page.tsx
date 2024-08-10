566f305c85e0201ae2eafaf43f54a9aa0a9a8d68
Yuvraj004 <yuvrajchat@gmail.com>
2024-08-10 23:02:07 +0530

    signup settings

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,26 +1,25 @@
-'use client';
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { useDebounceValue } from 'usehooks-ts'
-import { useToast } from "@/components/ui/use-toast"
+import { useDebounceValue } from "usehooks-ts";
+import { useToast } from "@/components/ui/use-toast";
 import { useRouter } from "next/navigation";
 import { signUpSchema } from "@/schemas/signUpSchema";
-import axios,{AxiosError} from 'axios';
+import axios, { AxiosError } from "axios";
 import { ApiResponse } from "@/types/ApiResponse";
 const page = () => {
   //fields create krne hain
-  
 
-  const [username, setusername] = useState('');
-  const [usernameMessage, setusernameMessage] = useState('');
+  const [username, setusername] = useState("");
+  const [usernameMessage, setusernameMessage] = useState("");
   //loader to manage state
-  const [isCheckingUsername,setIsCheckingUsername]=useState(false);
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
 
   //submit state management
-  const [isSubmitting,setIsSubmitting]=useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   //thodi thodi der mein mein check krenge ki username valid h ki nhi using APIs
   //isi ko debouncing kaha jata h
@@ -33,40 +32,77 @@ const page = () => {
   //zod implementation
   const form = useForm<z.infer<typeof signUpSchema>>({
     //using zod resolver and signupschema
-    resolver:zodResolver(signUpSchema),
-    defaultValues:{
-      username:'one',
-      email:'',
-      password:''
-    }
-    
-  })
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "one",
+      email: "",
+      password: "",
+    },
+  });
 
   useEffect(() => {
-    const checkUsernameUnique = async () =>{
-      if (debouncedUsername){
-        setIsCheckingUsername(true)
-        setusernameMessage('')
+    const checkUsernameUnique = async () => {
+      if (debouncedUsername) {
+        setIsCheckingUsername(true);
+        setusernameMessage("");
         try {
-          const response = await axios.get(`/api/check-username-unique?username=${debouncedUsername}`);
-          setusernameMessage(response.data.message)
+          const response = await axios.get(
+            `/api/check-username-unique?username=${debouncedUsername}`,
+          );
+          setusernameMessage(response.data.message);
         } catch (error) {
           const axiosError = error as AxiosError<ApiResponse>;
-          setusernameMessage(axiosError.response?.data.message ?? "Error checking username")
-        } finally{
-          setIsCheckingUsername(false)
+          setusernameMessage(
+            axiosError.response?.data.message ?? "Error checking username",
+          );
+        } finally {
+          setIsCheckingUsername(false);
         }
       }
+    };
+    checkUsernameUnique();
+  }, [debouncedUsername]);
 
+  //using zod to infer the type of data
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true);
+    try {
+      console.log(data);
+      const response = await axios.post<ApiResponse>("/api/sign-up", data);
+      toast({
+        title: "Success",
+        description: response.data.message,
+      });
+
+      router.replace(`/verify/${username}`);
+      setIsSubmitting(false);
+    } catch (error) {
+      console.error("Eror in signup of user", error);
+      const axiosError = error as AxiosError<ApiResponse>;
+      let errorMessage = axiosError.response?.data.message;
+      toast({
+        title: "Signup failed",
+        description: errorMessage,
+        variant: "destructive",
+      });
+      setIsSubmitting(false);
     }
-    checkUsernameUnique()
-  }, [debouncedUsername])
-  
-  const onSubmit = async (data) =>{}
-  
-  return (
-    <div>page</div>
-  )
-}
+  };
 
-export default page
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join Mystery Mesage
+          </h1>
+        </div>
+        <div>
+        
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;

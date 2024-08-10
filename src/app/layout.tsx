527feffa1ce49c41a228a4b83622057c1d08e5db
Yuvraj004 <yuvrajchat@gmail.com>
2024-08-10 23:44:54 +0530

    signup errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
-
 const inter = Inter({ subsets: ["latin"] });
 import { Toaster } from "@/components/ui/toaster"
 
@@ -18,10 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
-        <body className={inter.className}>{children}
+      <AuthProvider >
+        <>
+        <body className={inter.className}>
+          {children}
           <Toaster/>
-        </body>
+          </body>
+        </>
         
       </AuthProvider>
     </html>

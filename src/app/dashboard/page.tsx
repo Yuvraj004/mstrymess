4584c7e225e9d40c300447899619ab0a292fa9de
Yuvraj004 <yuvrajchat@gmail.com>
2024-08-14 20:06:32 +0530

    dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,19 @@
 'use client';
 
+import MessageCard from "@/components/MessageCard";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Switch } from "@/components/ui/switch";
 import { useToast } from "@/components/ui/use-toast";
 import { Message } from "@/model/userModel";
 import { acceptMessageSchema } from "@/schemas/acceptMessageSchema";
 import { ApiResponse } from "@/types/ApiResponse";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios, { AxiosError } from "axios";
+import { Loader2, RefreshCcw } from "lucide-react";
+import { User } from "next-auth";
 import { useSession } from "next-auth/react";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
 const Dashboard = () => {
@@ -71,11 +77,114 @@ const Dashboard = () => {
         variant:"destructive"
       })
     } finally {
+      setIsLoading(false)
       setIsSwitchLoading(false);
     }
-  },[])
+  }, [setIsLoading, setMessages])
+  
+  useEffect(()=>{
+    if (!session || !session.user) return
+    fetchMessages(true)
+    fetchAcceptMess()
+  }, [session, setValue, fetchAcceptMess, fetchMessages])
+  
+  //handle switch change
+  const handleSwitchChange = async () => {
+    try {
+      const res = await axios.post<ApiResponse>('/api/accept-message', {
+        acceptMessages: !acceptMessages
+      })
+      
+      setValue('acceptMessages', !acceptMessages)
+      toast({
+        title: res.data.message,
+        variant:'default'
+      })
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast({
+        title: "Error",
+        description:axiosError.response?.data.message || 'Failed to fetch message settings',
+        variant:'destructive'
+      })
+    }
+  }
+  
+  
+  //creating your url
+  const { username } = session?.user as User;
+  const baseuri = `${window.location.protocol}//${window.location.host}`;
+  const profileUrl = `${baseuri}/u/${username}`;
+  
+  //copy to clipboard function
+  const copyToClipboard = ()=>{
+    navigator.clipboard.writeText(profileUrl);
+    toast({
+      title: 'Url Copied',
+      description: 'Profile Url has been copied to your clipboard'
+    })
+  }
+  if (!session || !session.user) {
+    return <div>Please Login</div>
+  }
   return (
-    <div>Dashboard</div>
+    <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+      <h1 className="text-4xl font-bold mb-4">User Dashboard</h1>
+
+      <div className="mb-4">
+        <h2 className="text-lg font-semibold mb-2">Copy Your Unique Link</h2>{' '}
+        <div className="flex items-center">
+          <input
+            type="text"
+            value={profileUrl}
+            disabled
+            className="input input-bordered w-full p-2 mr-2"
+          />
+          <Button onClick={copyToClipboard}>Copy</Button>
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <Switch
+          {...register('acceptMessages')}
+          checked={acceptMessages}
+          onCheckedChange={handleSwitchChange}
+          disabled={isswitchloading}
+        />
+        <span className="ml-2">
+          Accept Messages: {acceptMessages ? 'On' : 'Off'}
+        </span>
+      </div>
+      <Separator />
+
+      <Button
+        className="mt-4"
+        variant="outline"
+        onClick={(e) => {
+          e.preventDefault();
+          fetchMessages(true);
+        }}
+      >
+        {isLoading ? (
+          <Loader2 className="h-4 w-4 animate-spin" />
+        ) : (
+          <RefreshCcw className="h-4 w-4" />
+        )}
+      </Button>
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
+        {messages.length > 0 ? (
+          messages.map((message, index) => (
+            <MessageCard
+              key={message?._id}
+              message={message}
+              onMessageDelete={handleDeleteMessage}
+            />
+          ))
+        ) : (
+          <p>No messages to display.</p>
+        )}
+      </div>
+    </div>
   )
 }
 
